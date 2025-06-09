3b3447e4811474047f9dcbe49afd4378e43be215
BunSkillet <bunskillet@gmail.com>
2025-06-09 23:41:04 +1000

    Add console log

--- server/app.js
+++ server/app.js
@@ -190,3 +190,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+console.log('Serving static files from:', path.join(__dirname, '../public'));
